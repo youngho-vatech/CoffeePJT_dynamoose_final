c28de0b0da7169cd18e3566c710737266e229533
영호김 <youngho.kim@vatech.com>
2021-01-18 16:10:11 +0900

    pagination commit

--- server/resolvers/user/allUsers.js
+++ server/resolvers/user/allUsers.js
@@ -3,7 +3,11 @@ const User = require("../../model/user")
 
 
 module.exports = async() => {
-    const result = await User.scan().exec()
+    const lastKey={
+        id: '377795c0-5928-11eb-8742-c78a85d8788e',
+        createdAt: '1610931482908'
+      }
+    const result = await User.scan().startAt(lastKey).limit(5).exec()
     console.log(result)
     return result
 }
